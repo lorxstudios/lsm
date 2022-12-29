6bfd51b9ed4e186c4446bfdc905a51e3f6fa4ca0
yzde <uwuzide@gmail.com>
2022-12-29 22:30:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import colorama
+from colorama import Fore, Back, Style
+import requests
 import os
 def sexit(x=""):
     if os.path.exists("exit.uwu"):
@@ -12,5 +15,25 @@ def sexit(x=""):
         f.write(x)
     f.close()
     exit()
-
+def urlme(x):
+    URLpred = "https://raw.githubusercontent.com/lorxstudios/lsm/main/main.py"
+    URL = x
+    response = requests.get(URL)
+    data = response.text
+    return(data)
+print("LSMe")
+print("Lorx Story Maker en español")
+print("------------------------------------------------")
+print("")
+print("")
+print("")
+print("")
+print("-> Buscando Actualizaciones.")
+rev = urlme("https://raw.githubusercontent.com/lorxstudios/lsm/main/main.py")
+xd = open("main.py", "r")
+if rev != xd.read():
+    xd.close()
+else:
+    xd.close()
+    print(Back.GREEN + "-> no hay actualizaciones pendientes" +  Style.RESET_ALL)
 sexit(input("->"))
